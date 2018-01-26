128a763e5bdada8133c1f914a45461267546e10c
Sepctre <sepctre@amazingsurge.com>
2018-01-26 16:30:09 +0800

    update generateSvgs and optimize

--- scripts/utils/generateSvgs.js
+++ scripts/utils/generateSvgs.js
@@ -2,15 +2,16 @@
 
 'use strict';
 
-let path = require('path');
-let fs = require('graceful-fs');
-let Promise = require("bluebird");
-let indexBy = require('lodash.indexby');
-let readFile = Promise.promisify(require("fs").readFile);
-let parseXml = Promise.promisify(require("xml2js").parseString);
-let mkdirp = require("mkdirp");
-let SVGO = require('svgo');
-let extend = require('extend');
+const pathModule = require('path');
+const fs = require('graceful-fs');
+const Promise = require("bluebird");
+const indexBy = require('lodash.indexby');
+const readFile = Promise.promisify(require("fs").readFile);
+const parseXml = Promise.promisify(require("xml2js").parseString);
+const mkdirp = require("mkdirp");
+const SVGO = require('svgo');
+const extend = require('extend');
+const colors = require('colors');
 
 // getGlyphs
 function hexToDec(hex) {
@@ -41,10 +42,108 @@ function getGlyphs(file) {
     });
 }
 
+// generateSvg
+var PIXEL = 128;
+
+function optionsForSize(pixelWidth, pixelHeight, width, height, addPadding) {
+  var horizontalPadding = 0;
+  var verticalPadding = 0;
+
+  if(addPadding && width && height) {
+    var vPad = parseInt(height / pixelHeight)*pixelHeight;
+    var scaledHeight = (height / vPad)*pixelHeight;
+    
+    verticalPadding = scaledHeight - pixelHeight;
+    if(verticalPadding > 2) {
+      verticalPadding = 0;
+    }
+  }
+  
+  let paddingTop = (parseInt(verticalPadding / 2)) * PIXEL;
+  let paddingBottom = verticalPadding*PIXEL - paddingTop;
+      
+  let paddingLeft = 0;
+  let paddingRight = 0;
+
+  if(pixelWidth === pixelHeight) {
+    paddingLeft = paddingTop;
+    paddingRight = paddingBottom;
+  }
+  
+  return {
+    paddingTop,
+    paddingBottom,
+    paddingLeft,
+    paddingRight
+  };
+}
+
+function getIconSvg(params, size) {
+  let {path, color, advWidth} = params;
+  const PIXEL_WIDTH = advWidth > 2048 ? 18 : (advWidth > 1792 ? 16 : 14);
+  const PIXEL_HEIGHT = 14;
+  
+  const BASE_WIDTH = PIXEL_WIDTH * PIXEL;
+  const BASE_HEIGHT = PIXEL_HEIGHT * PIXEL;
+  
+  var options = optionsForSize(PIXEL_WIDTH, PIXEL_HEIGHT, 
+    parseInt((BASE_WIDTH / BASE_HEIGHT) * size), size,
+    params.addPadding);
+  let {paddingLeft, paddingTop, paddingRight, paddingBottom} = options;  
+  let shiftX = -(-(BASE_WIDTH - advWidth)/2 - paddingLeft);
+  let shiftY = -(-2*PIXEL - paddingTop);  
+  let width = BASE_WIDTH + paddingLeft + paddingRight;
+  let height = BASE_HEIGHT + paddingBottom + paddingTop;
+  
+  const result =
+`<svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">
+  <g transform="translate(${shiftX} ${shiftY})">
+  <g transform="scale(1 -1) translate(0 -1280)">
+  <path d="${path}" fill="${color}" />
+  </g></g>
+</svg>`;
+
+  return result;
+}
+
+var svgo = new SVGO({
+  removeViewBox: true
+});
+
+function generateSvg(name, params) {
+  let svgFolder = pathModule.join(params.destFolder, 'icons');
+  mkdirp.sync(svgFolder);
+
+  return new Promise(function(resolve, reject) {
+    var outSvg = fs.createWriteStream(pathModule.join(svgFolder, name + '.svg'));
+    svgo.optimize(getIconSvg(params)).then(function(result) {
+      outSvg.write('<?xml version="1.0" encoding="utf-8"?>' +'\n'); 
+      outSvg.end(result.data);
+      resolve();
+    })
+  });
+}
+
+function generateIcon(params) {
+  var name = params.name;
+  console.log('Generating', name);
+  var workChain = [];
+  if (params.generateSvg) {
+    workChain.push(generateSvg(name, params));
+  }
+  return Promise.all(workChain).then(function() {
+    return {name: name}
+  })
+}
+
+function flatten(arr) {
+  return arr.reduce(function(a, b) {
+    return a.concat(b);
+  }, []);
+}
+
 module.exports = function(dest, filename, options) {
-  getGlyphs(`${dest}/${filename}.svg`).then(function(fontData) {
-    // console.log(options.icons);
-  	// console.info(data);
+  return getGlyphs(`${dest}/${filename}.svg`).then(function(fontData) {
     let icons = new Array();
     for (let i in options.icons) {
       if (options.icons[i].content != undefined) {
@@ -64,5 +163,25 @@ module.exports = function(dest, filename, options) {
       }
     }
     return icons;
+  }).then(function(glyphs) {
+    let work = [];
+
+    let iconConfigs = flatten(glyphs.map(function (glyph) {
+      return extend(true, {}, {
+        name: glyph.name,
+        advWidth: glyph.data['horiz-adv-x'] || 1536,
+        path: glyph.data.d,
+        generateSvg: true,
+        destFolder: dest
+      });
+    }));
+    
+    work.push(iconConfigs.map(function(params) {
+      generateIcon(params);
+    }));
+
+    Promise.all(work).then(function() {
+      console.log(colors.green("All done!"));
+    });
   });
 }
